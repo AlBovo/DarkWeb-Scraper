c12118177a84180e96792c9063d3597347835ad8
mark-74 <balduccimarco2007@gmail.com>
2025-05-30 10:44:24 +0200

    added first part of dynamic list

--- interface.py
+++ interface.py
@@ -7,12 +7,13 @@ parser = argparse.ArgumentParser(
     description="Cmd interface for the application"
     )
 
-parser.add_argument('-d', '--domain', type=str, required=False)
-parser.add_argument('-n', '--name', type=str, required=False)
-parser.add_argument('-l', '--latest', type=bool, required=False, default=False)
-parser.add_argument('-s', '--static', action='store_true', required=False, default=False)
-parser.add_argument('-a', '--all', action='store_true', required=False, default=False)
-parser.add_argument('--verify', action='store_true', required=False, default=False)
+parser.add_argument('-d', '--domain', type=str, required=False, description="Domain to search on the dark web")
+parser.add_argument('-n', '--name', type=str, required=False, description="Name to search on the dark web")
+parser.add_argument('-l', '--latest', type=bool, required=False, default=False, description="If some websites have a latest page, only that page will be scraped")
+parser.add_argument('-s', '--static', action='store_true', required=False, default=False, description="Scrape static pages only")
+parser.add_argument('-a', '--all', action='store_true', required=False, default=False, description="Scrape all pages, including dynamic ones")
+parser.add_argument('-v', '--verbose', action='store_true', required=False, default=False, description="Enable verbose output")
+parser.add_argument('--verify', action='store_true', required=False, default=False, description="Verify onion URLs for accessibility")
     
 args = parser.parse_args()
 
